6c1e8b61fad180c12cffa325d89c5ae94b94c342
Mehmet Adem Şengül <adem.sengul@anadoluefes.com>
2021-04-05 15:50:07 +0300

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/main/java/com/turkcell/poc/PocApplication.java
+++ src/main/java/com/turkcell/poc/PocApplication.java
@@ -6,8 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PocApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PocApplication.class, args);
+    }
 
+    /*
+                List<Integer> intList = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            intList.add(i);
+        }
+        long startTime = new Date().getTime();
+        ForkJoinPool forkJoinPool = new ForkJoinPool(1);
+        final List<Integer> primes = forkJoinPool.submit(()-> 
+                intList.parallelStream().parallel()
+                        .filter(item -> item < 999999)
+                        .collect(Collectors.toList()) ).get();
+        System.out.println((new Date().getTime()-startTime));
+     */
 }
